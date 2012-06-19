6d6dd8c1999fcf7f494114f836109c15ff448340
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-06-19 18:19:47 +0100

    Download and Upload widgets are now implemented using UiBinder

---
+++ src/org/purewidgets/client/widgets/GuiDownload.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package org.purewidgets.client.widgets;
+
+import java.util.ArrayList;
+
+import org.purewidgets.client.widgets.feedback.InputFeedback;
+import org.purewidgets.shared.Log;
+import org.purewidgets.shared.events.InputEvent;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.*;
+
+/**
+ * 
+ * <h3>CSS Style Rules</h3>
+ * <dl>
+ * <dt>.instantplaces-GuiButton</dt>
+ * <dd>the &lt;button&gt; element</dd>
+ * <dt>.instantplaces-GuiButton-disabled</dt>
+ * <dd>the disabled &lt;button&gt; element</dd>
+ * <dt>.instantplaces-GuiButton .caption</dt>
+ * <dd>the caption of the button</dd>
+ * <dt>.instantplaces-GuiButton .referencecode</dt>
+ * <dd>the reference code element</dd>
+ * <dt>.instantplaces-GuiButton-disabled .caption</dt>
+ * <dd>the disabled caption element</dd>
+ * <dt>.instantplaces-GuiButton-disabled .referencecode</dt>
+ * <dd>the disabled reference code element</dd>
+ * </dl>
+ * 
+ * The component elements (.caption and .referencecode) also keep the GWT style
+ * name (gwt-Html).
+ * 
+ * @author Jorge C. S. Cardoso
+ * 
+ */
+public class GuiDownload extends GuiWidget {
+	@UiTemplate("GuiDownload.ui.xml")
+	interface GuiDownloadUiBinder extends UiBinder<Widget, GuiDownload> {	}
+	private static GuiDownloadUiBinder uiBinder = GWT.create(GuiDownloadUiBinder.class);
+
+
+	@UiField
+	HTMLPanel uiVerticalPanelMain;
+	
+	@UiField
+	Label uiHTMLCaption;
+	
+	@UiField
+	Label uiHTMLReferenceCode;
+	
+	private org.purewidgets.shared.widgets.DownloadButton widgetDownloadButton;
+
+	public GuiDownload(String widgetId, String caption, String url) {
+		this(widgetId, caption, url, null);
+	}
+
+	public GuiDownload(String widgetId, String caption, String url, String suggestedRef) {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		widgetDownloadButton = new org.purewidgets.shared.widgets.DownloadButton(widgetId, caption, url);
+
+		this.setWidget(widgetDownloadButton);
+		
+
+		//this.button = new com.google.gwt.user.client.ui.Button();
+
+		
+		this.uiHTMLCaption.setText(caption);
+		this.uiHTMLReferenceCode.setText(ReferenceCodeFormatter.format(this.getWidgetOptions()
+				.get(0).getReferenceCode()));
+//		
+//		// Give the overall composite a style name.
+//		this.setStyleName(DEFAULT_STYLENAME);
+//		this.lblCaption.addStyleName(GuiButton.CAPTION_STYLENAME_SUFFIX);
+//		this.lblReferenceCode.addStyleName(GuiButton.REFERENCECODE_STYLENAME_SUFFIX);
+		this.sendToServer();
+	}
+
+	
+	void handleClick(ClickEvent event) {
+		// Simulate an input event
+		InputEvent e = new InputEvent(GuiDownload.this.getWidgetOptions().get(0), null);
+		ArrayList<InputEvent> inputList = new ArrayList<InputEvent>();
+		inputList.add(e);
+
+		GuiDownload.this.widget.onInput(inputList);
+	}
+	
+	@Override
+	public InputFeedback<GuiDownload> handleInput(InputEvent ie) {
+		return null;
+	}
+
+	@Override
+	public void onReferenceCodesUpdated() {
+		// Log.debug(this + " Updating reference code");
+		this.uiHTMLReferenceCode.setText(ReferenceCodeFormatter.format(this.getWidgetOptions().get(0)
+				.getReferenceCode()));
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		Log.debug(this, "" + enabled);
+		super.setEnabled(enabled);
+		//this.uiButtonMain.setEnabled(enabled);
+	}
+
+	@Override
+	public void setSize(String w, String h) {
+		super.setSize(w, h);
+		this.uiVerticalPanelMain.setSize(w, h);
+	}
+
+}
--- src/org/purewidgets/client/widgets/GuiUpload.java
+++ src/org/purewidgets/client/widgets/GuiUpload.java
@@ -6,19 +6,25 @@ package org.purewidgets.client.widgets;
 
 import java.util.ArrayList;
 
+import org.purewidgets.client.widgets.GuiDownload.GuiDownloadUiBinder;
 import org.purewidgets.client.widgets.feedback.InputFeedback;
 import org.purewidgets.shared.Log;
 import org.purewidgets.shared.events.ActionEvent;
 import org.purewidgets.shared.events.InputEvent;
 import org.purewidgets.shared.widgets.Upload;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.FocusEvent;
 import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.Timer;
 
 
@@ -49,64 +55,24 @@ import com.google.gwt.user.client.Timer;
  * 
  * @author Jorge C. S. Cardoso
  */
-public class GuiUpload extends GuiWidget implements KeyPressHandler, FocusHandler {
+public class GuiUpload extends GuiWidget  {
+	@UiTemplate("GuiUpload.ui.xml")
+	interface GuiUploadUiBinder extends UiBinder<Widget, GuiUpload> {	}
+	private static GuiUploadUiBinder uiBinder = GWT.create(GuiUploadUiBinder.class);
 	protected final String DEFAULT_USER_INPUT_FEEDBACK_PATTERN = "%U% : %P[0]%";
 	
-	/**
-	 * The default style name for the TextBox widget.
-	 */
-	public static final String DEFAULT_STYLENAME = "instantplaces-GuiTextBox";
+	
 
-	/**
-	 * The suffix for the textbox control.
-	 */
-	public static final String TEXTBOX_STYLENAME_SUFFIX = "textbox";
-
-	/**
-	 * The suffix for the caption of the textbox.
-	 */
-	public static final String CAPTIONCONTAINER_STYLENAME_SUFFIX = "captioncontainer";
+	@UiField
+	HTMLPanel uiVerticalPanelMain;
 	
-	/**
-	 * The suffix for the caption of the textbox.
-	 */
-	public static final String CAPTIONCONTAINERCAPTION_STYLENAME_SUFFIX = "caption";
+	@UiField
+	Label uiHTMLCaption;
 	
-	/**
-	 * The suffix for the caption of the textbox.
-	 */
-	public static final String CAPTIONCONTAINERREFERENCECODE_STYLENAME_SUFFIX = "referencecode";
-
-	/**
-	 * The suffix for the caret.
-	 */
-	public static final String CARET_STYLENAME_SUFFIX = "caret";
+	@UiField
+	Label uiHTMLReferenceCode;
 	
-	
-	/**
-	 * The graphical textbox.
-	 */
-	com.google.gwt.user.client.ui.TextBox textBox;
-	
-	HTMLPanel htmlPanel;
-	Label lblFlashingCaret;
-	private HorizontalPanel captionContainer;
-	private Label lblCaption;
-	private Label lblReferenceCode;
-	
-	
-	Timer caretTimer;
-	boolean isCaretOn;
-	String caretOn = "|";
-	String caretOff = "";
-	
-	@SuppressWarnings("unused")
-	private org.purewidgets.shared.widgets.Upload widgetTextBox;
-	
-	/**
-	 *  The input text
-	 */
-	private String text = "";
+	private org.purewidgets.shared.widgets.Upload widgetUpload;
 	
 	public GuiUpload(String caption) {
 		this((String)null, caption);
@@ -117,98 +83,35 @@ public class GuiUpload extends GuiWidget implements KeyPressHandler, FocusHandle
 	}
 		
 	public GuiUpload(String widgetId, String caption, String suggestedReference) {
-		this(new org.purewidgets.shared.widgets.Upload(widgetId, caption), suggestedReference);
-		
-	}
-	
-	public GuiUpload(Upload widgetTextBox, String suggestedReference) {
 		super();
-
+		
+		this.widgetUpload = new org.purewidgets.shared.widgets.Upload(widgetId, caption);
+		
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		 
 		
 		/*
 		 * Set the default user feedback pattern
 		 */
 		this.userInputFeedbackPattern = DEFAULT_USER_INPUT_FEEDBACK_PATTERN;
 		
-		this.widgetTextBox = widgetTextBox;
-		this.setWidget(widgetTextBox);
+		this.uiHTMLCaption.setText(caption);
+		this.setWidget(widgetUpload);
 		//this.sendToServer();
 		
-		
-	    /*
-	     * Gui stuff
-	     */
-		textBox = new com.google.gwt.user.client.ui.TextBox();
-		
-		textBox.addKeyPressHandler(this);
-		textBox.addFocusHandler(this);
-		//textBox.setText(caption);
-		
-		//setLblCaption( new Label( this.widgetTextBox.getCaption() ) );
-		
-		
-		
-		lblReferenceCode = new Label( ReferenceCodeFormatter.format( this.getWidgetOptions().get(0).getReferenceCode() ) );
-		
-		lblFlashingCaret = new Label("I");
-		
-		captionContainer = new HorizontalPanel();
-	//	captionContainer.add(this.getLblCaption());
-		captionContainer.add(this.lblReferenceCode);
-		
-		htmlPanel = new HTMLPanel("<div id='textbox'></div><div id='caret'></div><div id='caption'></div>");
-		
-		
-		htmlPanel.addAndReplaceElement(textBox, "textbox");
-		htmlPanel.addAndReplaceElement(captionContainer, "caption");
-		//htmlPanel.addAndReplaceElement(lblReferenceCode, "referencecode");
-		htmlPanel.addAndReplaceElement(lblFlashingCaret, "caret");
-		
-		//setInputFeedbackDisplay(new InputFeedbackPanel());
-		initWidget(htmlPanel);
-		
-	    // Give the overall composite a style name.
-	    setStyleName(DEFAULT_STYLENAME);
-		this.textBox.addStyleName(GuiUpload.TEXTBOX_STYLENAME_SUFFIX);
-		this.captionContainer.addStyleName(GuiUpload.CAPTIONCONTAINER_STYLENAME_SUFFIX);
-		this.lblFlashingCaret.addStyleName(GuiUpload.CARET_STYLENAME_SUFFIX);
-		this.lblReferenceCode.addStyleName(GuiUpload.CAPTIONCONTAINERREFERENCECODE_STYLENAME_SUFFIX);
-		//this.getLblCaption().addStyleName(GuiUpload.CAPTIONCONTAINERCAPTION_STYLENAME_SUFFIX);
-		
+	
 		
 		this.sendToServer();
 	}
 	
 		
 	
-
-	@Override
-	public void onKeyPress(KeyPressEvent event) {
-		if (event.getCharCode() == 13) {
-			Log.debug(this, "Enter detected");
-			
-			ArrayList<String> params = new ArrayList<String>();
-			params.add(this.textBox.getText());
-			
-			InputEvent e = new InputEvent(this.getWidgetOptions().get(0), params);
-			textBox.setText("");
-			
-			// remove the focus so that the internal caret disappears.
-			textBox.setFocus(false);
-			
-			ArrayList<InputEvent> inputList = new ArrayList<InputEvent>();
-			inputList.add(e);
-			this.widget.onInput(inputList);
-			//this.onInput(inputList);	
-		}
-		
-	}
-	
 	@Override
 	public void onReferenceCodesUpdated() {
-		if (!(this.lblReferenceCode == null)) {
-			this.lblReferenceCode.setText( ReferenceCodeFormatter.format( this.getWidgetOptions().get(0).getReferenceCode() ) );
-		}
+		// Log.debug(this + " Updating reference code");
+		this.uiHTMLReferenceCode.setText(ReferenceCodeFormatter.format(this.getWidgetOptions().get(0)
+				.getReferenceCode()));
 	}
 	
 	@Override
@@ -231,25 +134,13 @@ public class GuiUpload extends GuiWidget implements KeyPressHandler, FocusHandle
 		return feedback;
 	}		
 
-
-	@Override
-	public void onFocus(FocusEvent event) {
-		//this.textBox.setFocus(false);
-		
-	}
-	
-	
-	public void setText(String text) {
-		this.textBox.setText(text);
-		this.text = text;
-	}
 	
 
 	
 	@Override
 	public void setEnabled(boolean enabled) {
 		Log.debug("" + inputEnabled);
-		textBox.setEnabled(enabled);
+		
 		//setCaret(enabled);
 		super.setEnabled(enabled);
 	}
@@ -258,90 +149,5 @@ public class GuiUpload extends GuiWidget implements KeyPressHandler, FocusHandle
 	public void setInputEnabled(boolean inputEnabled) {
 		super.setInputEnabled(inputEnabled);
 	}
-	/*
-	@Override
-	public void onInput(InputEvent ie) {
-		
-		InputFeedback f = new InputFeedback(ie);
 	
-		this.getFeedbackSequencer().add(f);
-		
-	}
-	
-	@Override
-	public void start(InputFeedback inputFeedback) {
-		Log.debug("");
-		
-		// Disable the caret when showing feedback
-		this.setCaret(false);
-		
-		InputFeedback.Type type;
-		InputEvent ie = inputFeedback.getInputEvent();
-		String text = "";
-
-		if (ie.getCommand() != null && ie.getCommand().getParameter(0) != null) {
-			type = this.isInputEnabled()?InputFeedback.Type.ACCEPTED:InputFeedback.Type.NOT_ACCEPTED;
-			text = ie.getCommand().getParameter(0);
-			if (type == InputFeedback.Type.ACCEPTED) {
-				inputFeedback.setInfo(ie.getIdentity().getName()+": "+text);
-			} else {
-				inputFeedback.setInfo(ie.getIdentity().getName());
-			}
-		} else {
-			type = InputFeedback.Type.NOT_ACCEPTED;
-			if (this.isInputEnabled()) { // not accepted because of parameter error
-				inputFeedback.setInfo(ie.getIdentity().getName() + ": " + constants.textboxEmptyTextError());
-			} else {
-				inputFeedback.setInfo(ie.getIdentity().getName());
-			}
-		}
-		inputFeedback.setType(type);
-		
-		this.text = text;
-	
-		this.getInputFeedbackDisplay().show(inputFeedback);
-
-		// Only fire application event if the input was accepted
-		if (inputFeedback.getType() == InputFeedback.Type.ACCEPTED) {
-			ActionEvent ae = new ActionEvent(ie.getIdentity(), this, ie.getWidgetOptionID(), text);
-			this.fireActionEvent(ae);
-		}
-	}
-	
-@Override
-public void stop(InputFeedback feedback, boolean noMore) {
-	super.stop(feedback, noMore);
-	
-	// if there's no more feedback waiting, turn the caret on
-	if (noMore) {
-		this.setCaret(this.isInputEnabled());
-	}
-}
-*/
-
-	/**
-	 * @return the text
-	 */
-	public String getText() {
-		return text;
-	}
-
-
-	/**
-	 * @param lblCaption the lblCaption to set
-	 */
-	public void setLblCaption(Label lblCaption) {
-		this.lblCaption = lblCaption;
-	}
-
-
-	/**
-	 * @return the lblCaption
-	 */
-	public Label getLblCaption() {
-		return lblCaption;
-	}
-
-
-
 }
