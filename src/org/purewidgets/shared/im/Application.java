7165e18d4dbcfd582b1851e3f2e87139c669960a
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-06-22 16:13:37 +0100

    Fixed error introduced with temporary changes

--- src/org/purewidgets/shared/im/Application.java
+++ src/org/purewidgets/shared/im/Application.java
@@ -3,7 +3,7 @@
  */
 package org.purewidgets.shared.im;
 
-import org.purewidgets.client.json.GenericJson;
+
 
 /**
  * @author "Jorge C. S. Cardoso"
@@ -71,45 +71,5 @@ public class Application {
 		this.applicationBaseUrl = applicationBaseUrl;
 	}
 
-
-	public class JsApplication extends GenericJson {
-		// Overlay types always have protected, zero-arg ctors
-		   protected JsApplication() { 
-			   
-		   }
-
-
-		   
-			public final native void setPlaceId(String placeId)  /*-{ 
-				this.placeId = placeId; 
-			}-*/;
-
-
-			public final native String getPlaceId() /*-{ 
-				return this.placeId; 
-			}-*/;
-			
-			  
-
-			public final native void setApplicationId(String appId)  /*-{ 
-				this.applicationId = appId; 
-			}-*/;
-
-
-			public final native String getApplicationBaseUrl() /*-{ 
-				return this.applicationBaseUrl; 
-			}-*/;
-			
-
-			public final native void setApplicationBaseUrl(String baseUrl)  /*-{ 
-				this.applicationBaseUrl = baseUrl; 
-			}-*/;
-
-
-			public final native String getApplicationId() /*-{ 
-				return this.applicationId; 
-			}-*/;
-	}
-	
 	
 }
