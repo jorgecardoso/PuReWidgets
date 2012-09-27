4254d22f307069d272dff87a4966a6807f1a913c
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-09-27 11:34:11 +0100

    Added javadoc to client.PDApplicationLifeCycle interface.

--- src/org/purewidgets/client/application/PDApplicationLifeCycle.java
+++ src/org/purewidgets/client/application/PDApplicationLifeCycle.java
@@ -4,9 +4,20 @@
 package org.purewidgets.client.application;
 
 /**
+ * This interface defines the life-cycle methods that a public display application goes through in the
+ * course of its execution.
+ * 
+ * GWT applications that are meant to run as public display applications must implement this interface.
+ * 
  * @author "Jorge C. S. Cardoso"
  *
  */
 public interface PDApplicationLifeCycle {
+	
+	/**
+	 * Triggers the loaded event after the corresponding PDApplication has been fully loaded.
+	 * 
+	 * @param pdApplication
+	 */
 	public void onPDApplicationLoaded(PDApplication pdApplication);
 }
