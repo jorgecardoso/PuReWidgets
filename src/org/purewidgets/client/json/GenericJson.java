1197e4603ffb922352c3c59a217f0d352cf318de
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-09-28 23:58:19 +0100

    Cleaned and javadoc'd the package org.purewidgets.client.json package.

--- src/org/purewidgets/client/json/GenericJson.java
+++ src/org/purewidgets/client/json/GenericJson.java
@@ -2,23 +2,49 @@ package org.purewidgets.client.json;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * GenericJson provides auxiliary methods common to all Json DTO objects.
+ * 
+ * @author "Jorge C. S. Cardoso"
+ *
+ */
 public abstract class GenericJson extends JavaScriptObject {
 
 	// Overlay types always have protected, zero-arg ctors
-	  protected GenericJson() { }
-	  
-	  public final static native <T> T getNew() /*-{
-	  	return new Object();
-	  }-*/; 
-	  
-	  public final static native <T> T fromJson(String json) /*-{
-	  	return eval('('+json+')');
-	  }-*/;
-	  
-	public final native String toJsonString() /*-{
-	  return JSON.stringify(this);
+	protected GenericJson() {
+	}
+
+	/**
+	 * Gets a new JavaScriptObject of the specified class.
+	 * @return
+	 */
+	public final static native <T> T getNew() /*-{
+		return new Object();
 	}-*/;
-	
+
+	/**
+	 * Convers a JSON string to a JavaScriptObject of the specified class.
+	 * @param json The JSON string.
+	 * @return A JavaScriptObject 
+	 */
+	public final static native <T> T fromJson(String json) /*-{
+		return eval('(' + json + ')');
+	}-*/;
+
+	/**
+	 * Converts this object to a JSON string.  
+	 * @return
+	 */
+	public final native String toJsonString() /*-{
+		return JSON.stringify(this);
+	}-*/;
+
+	/**
+	 * Converts a JavaScriptObject to a JSON string.
+	 * 
+	 * @param jso The JavaScriptObject to convert.
+	 * @return The JSON string 
+	 */
 	public final static native String toJsonString(JavaScriptObject jso) /*-{
 		return JSON.stringify(jso);
 	}-*/;
