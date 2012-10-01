43602131d3731cdd8bf76ed26680de3363dc9110
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-10-01 15:08:18 +0100

    Javadoc'd the org.purewidgets.shared.exceptions.HttpServerException.

--- src/org/purewidgets/shared/exceptions/HttpServerException.java
+++ src/org/purewidgets/shared/exceptions/HttpServerException.java
@@ -16,10 +16,17 @@ public class HttpServerException extends Exception implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates a new HttpServerException without a message.
+	 */
 	public HttpServerException() {
 		super();
 	}
 	
+	/**
+	 * Creates a new HttpServerException with the specified error message.
+	 * @param message The error message associated with the exception.
+	 */
 	public HttpServerException(String message) {
 		super(message);
 	}
